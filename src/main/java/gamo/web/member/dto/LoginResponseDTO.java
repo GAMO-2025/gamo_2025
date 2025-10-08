baa878c4f54ecd8ceb3b71f4339056053d0e4b12
1siis1 <sisees01@gmail.com>
2025-10-08 18:38:36 +0900

    [CHORE] LoginResponseDTOO, Member 엔티티 수정

--- src/main/java/gamo/web/member/dto/LoginResponseDTO.java
+++ src/main/java/gamo/web/member/dto/LoginResponseDTO.java
@@ -5,13 +5,11 @@ import lombok.Getter;
 
 @Getter
 public class LoginResponseDTO {
-    private final Long id;
     private final String name;
     private final String email;
     private final String picture;
 
     public LoginResponseDTO(Member member) {
-        this.id = member.getId();
         this.name = member.getName();
         this.email = member.getEmail();
         this.picture = member.getProfileImage();
