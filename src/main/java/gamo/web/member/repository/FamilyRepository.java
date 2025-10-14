16d241d50219f2ed3c508fa4d78009578ad72cfc
yjhss <20211420@sungshin.ac.kr>
2025-10-14 21:05:43 +0900

    [REFACTOR] repository 어노테이션 삭제

--- src/main/java/gamo/web/member/repository/FamilyRepository.java
+++ src/main/java/gamo/web/member/repository/FamilyRepository.java
@@ -4,6 +4,5 @@ import gamo.web.member.domain.Family;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface FamilyRepository extends JpaRepository<Family, Long> {
 }
