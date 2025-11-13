41ec920994bc1dba4e5cbdc6708ad956c957c55e
1siis1 <sisees01@gmail.com>
2025-11-13 13:56:23 +0900

    [FEAT] 가ì¡IX] ê가족 중복 초대  버그 수정

--- src/main/java/gamo/web/family/controller/FamilyController.java
+++ src/main/java/gamo/web/family/controller/FamilyController.java
@@ -6,6 +6,7 @@ import gamo.web.member.service.MemberService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -22,14 +23,16 @@ public class FamilyController {
     @PostMapping("/invite")
     public ResponseEntity<Map<String, String>> inviteFamily(@AuthenticationPrincipal UserPrincipal user) {
         Long memberId = memberService.getMemberId(user);
-       if(familyService.hasFamily(memberId)) {
+        // 이미 가족에 가입되어 있는 사람이라면, 그 가족의 코드를 보여준다
+        if(familyService.hasFamily(memberId)) {
            String code =  familyService.getFamilyCode(memberId);
            return ResponseEntity.ok().body(Map.of("familyCode", code));
-       }
+        }
 
-       familyService.inviteFamily(memberId);
-       String code =  familyService.getFamilyCode(memberId);
-       return ResponseEntity.ok().body(Map.of("familyCode", code));
+        // 아니라고 하면, 코드 새로 생성해서 그걸 보내줌
+        familyService.inviteFamily(memberId);
+        String code =  familyService.getFamilyCode(memberId);
+        return ResponseEntity.ok().body(Map.of("familyCode", code));
     }
 
     @Getter
@@ -38,11 +41,17 @@ public class FamilyController {
         private String inviteCode;
     }
 
+    // 가족 코드 입력해서 그 가족에 들어가기(초대받기)
     @PostMapping("/join")
-    public ResponseEntity<Void> joinFamily(@AuthenticationPrincipal UserPrincipal user,
+    public ResponseEntity<Map<String, String>> joinFamily(@AuthenticationPrincipal UserPrincipal user,
                                            @RequestBody InviteCode inviteCode) {
         Long memberId = memberService.getMemberId(user);
+
+        if (familyService.hasFamily(memberId)) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(Map.of("message", "이미 가입된 가족이 있습니다."));
+        }
         familyService.joinFamily(memberId, inviteCode.getInviteCode());
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(Map.of("message", "가족 참여 완료!"));
     }
 }
