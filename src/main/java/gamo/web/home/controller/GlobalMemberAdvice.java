1c9def19cef64de3ba8e0fefa7dd8fa22a0b45a4
yjhss <20211420@sungshin.ac.kr>
2025-11-27 16:49:31 +0900

    [FIX] GlobalMemberAdvice에서 DB 호출 없이 UserPrincipal의 Member 사용하도록 수정

--- src/main/java/gamo/web/home/controller/GlobalMemberAdvice.java
+++ src/main/java/gamo/web/home/controller/GlobalMemberAdvice.java
@@ -1,6 +1,7 @@
 package gamo.web.home.controller;
 
 import gamo.web.auth.UserPrincipal;
+import gamo.web.member.domain.Member;
 import gamo.web.member.dto.LoginResponseDTO;
 import gamo.web.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,14 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
-@RequiredArgsConstructor
 public class GlobalMemberAdvice {
-
-    private final MemberService memberService;
 
     @ModelAttribute("member")
     public LoginResponseDTO addLoggedInMember(@AuthenticationPrincipal UserPrincipal user) {
         if (user == null) return null;
-        Long memberId = memberService.getMemberId(user);
-        return memberService.getMyInfo(memberId);
+
+        // DB 호출 없이 UserPrincipal에 있는 Member 사용
+        Member member = user.getMember();
+        return new LoginResponseDTO(member);
     }
 }
