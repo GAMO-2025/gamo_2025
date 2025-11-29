bcf7b5987e73d2fad4a73433a7ad706310a17982
1siis1 <sisees01@gmail.com>
2025-11-29 17:45:35 +0900

    [FIX]도메인 필드명에 따른 repo 함수명 수정

--- src/main/java/gamo/web/photo/service/PhotoService.java
+++ src/main/java/gamo/web/photo/service/PhotoService.java
@@ -55,7 +55,7 @@ public class PhotoService {
 
     @Transactional(readOnly = true)
     public String getLatestAlbumThumbnailByFamily(Long familyId) {
-        Album latestAlbum = albumRepository.findTop1ByFamilyIdOrderByAlbumIdDesc(familyId)
+        Album latestAlbum = albumRepository.findTop1ByFamilyIdOrderByAlbum_idDesc(familyId)
                 .orElse(null);
 
         if (latestAlbum == null) {
