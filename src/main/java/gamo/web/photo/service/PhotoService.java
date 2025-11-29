37753190762bd0589a2fe9ab9c37e99794d1ec1a
chaehyeon02 <rhee2015@gmail.com>
2025-11-29 21:25:04 +0900

    [FIX] 사진 조회

--- src/main/java/gamo/web/photo/service/PhotoService.java
+++ src/main/java/gamo/web/photo/service/PhotoService.java
@@ -128,9 +128,12 @@ public class PhotoService {
     @Transactional(readOnly = true)
     public PhotoRequestDTO getPhotoById(Long memberId, Long photoId) {
         //권한 확인
-        Long memberOfPhoto = photoRepository.findMemberById(photoId);
-
-        if(!memberId.equals(memberOfPhoto))
+        Long familyOfPhoto = albumRepository.findFamilyIdById(photoRepository.findAlbumIdByPhotoId(photoId));
+        Long familyOfMember = memberRepository.findFamilyIdById(memberId)
+                .orElseThrow(() -> new CustomException((ErrorCode.FAMILY_NOT_FOUND)));
+        log.info("[PhotoService] photo의 family: {}", familyOfPhoto);
+        log.info("[PhotoService] member의 family: {}", familyOfMember);
+        if(!familyOfMember.equals(familyOfPhoto))
             throw new CustomException(ErrorCode.PHOTO_ACCESS_FORBIDDEN);
 
         Photo photo = photoRepository.findById(photoId).get();
