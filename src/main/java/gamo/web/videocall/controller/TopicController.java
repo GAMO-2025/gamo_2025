103d3ec0d5651867474fb2d4001e05e25e1e42c4
jiwonLee42 <ljw56b@gmail.com>
2025-12-01 04:37:13 +0900

    [FEAT] 주제 추천 API 연결

--- src/main/java/gamo/web/videocall/controller/TopicController.java
+++ src/main/java/gamo/web/videocall/controller/TopicController.java
@@ -1,4 +1,32 @@
 package gamo.web.videocall.controller;
 
+import gamo.web.auth.UserPrincipal;
+import gamo.web.videocall.dto.RecommendDTO;
+import gamo.web.videocall.service.VideoCallService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/topics")
+@RestController
 public class TopicController {
+
+    private final VideoCallService videoCallService;
+
+    @GetMapping("/recommended/{targetId}")
+    public RecommendDTO.KeywordResponse getRecommendedTopic
+            (@AuthenticationPrincipal UserPrincipal user,
+            @PathVariable Long targetId) {
+        return videoCallService.viewRecommendedKeywords(user.getMember(), targetId, 3);
+    }
+
+    @GetMapping("/home")
+    public RecommendDTO.HomeKeywordResponseDTO getHomeTopic(@AuthenticationPrincipal UserPrincipal user) {
+        return videoCallService.viewLatestRecommendedKeywords(user.getMember());
+    }
+
 }
--- src/main/java/gamo/web/videocall/dto/RecommendDTO.java
+++ src/main/java/gamo/web/videocall/dto/RecommendDTO.java
@@ -1,4 +1,48 @@
 package gamo.web.videocall.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
 public class RecommendDTO {
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    public static class RecommendRequest {
+
+        @JsonProperty("videocall_ids")
+        private List<Long> videoCallIds;
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Builder
+    public static class RecommendResponse {
+        private Integer status;
+        @JsonProperty("recommended_topic")
+        private String recommendedTopic;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class KeywordResponse {
+        private boolean success;
+        private String topic;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    @NoArgsConstructor
+    public static class HomeKeywordResponseDTO {
+        private String profileImage;
+        private String name;
+        private String topic;
+    }
+
 }
