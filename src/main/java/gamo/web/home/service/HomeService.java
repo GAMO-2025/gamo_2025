b6077bdf6b2a12384d29043be92794bb71e37935
1siis1 <sisees01@gmail.com>
2025-12-01 14:01:52 +0900

    Revert "Merge pull request #57 from GAMO-2025/feat/home-ajenda"
    This reverts commit 297f7fa74626b7a2f29ae7521a9a6c543e189b1d, reversing
    changes made to f70e3f24bb1467f802b9efed608aa9be017bde80.

--- src/main/java/gamo/web/home/service/HomeService.java
+++ src/main/java/gamo/web/home/service/HomeService.java
@@ -5,7 +5,8 @@ import gamo.web.letter.dto.LetterCountDTO;
 import gamo.web.letter.service.LetterService;
 import gamo.web.member.domain.Member;
 import gamo.web.photo.service.PhotoService;
-import gamo.web.videocall.dto.RecommendDTO;
+import gamo.web.videocall.dto.VideoCallListResponse;
+import gamo.web.videocall.dto.VideoCallResponseDTO;
 import gamo.web.videocall.service.VideoCallService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,16 +19,13 @@ public class HomeService {
     private final LetterService letterService;
     private final PhotoService photoService;
 
-
     @Transactional(readOnly = true)
     public HomeSummaryDTO getHomeSummary(Member member) {
-
+        VideoCallListResponse vclr = videoCallService.viewVideoCallHistory(member, 1);
         LetterCountDTO lcDTO = letterService.getLetterCounts(member.getId());
-        long unreadLetterCount = lcDTO.getUnreadCount();
 
         String targetNickname = null;
         String targetProfileImage = null;
-
         String albumThumbnail = null;
 
         //앨범 최근꺼 하나 가져오기
@@ -38,18 +36,16 @@ public class HomeService {
 
         String ajenda = "추후 추가예정";
 
+        long unreadLetterCount = lcDTO.getUnreadCount();
 
 
+        // 가장 최근 통화자 정보 가져오기!(1명)
+        if (vclr != null && vclr.getContent() != null && !vclr.getContent().isEmpty()) {
+            VideoCallResponseDTO latestCall = vclr.getContent().get(0);
 
-        RecommendDTO.HomeKeywordResponseDTO homeKeyword =
-                videoCallService.viewLatestRecommendedKeywords(member);
-
-        if (homeKeyword != null) {
-            targetNickname = homeKeyword.getName();
-            targetProfileImage = homeKeyword.getProfileImage();
-            ajenda = homeKeyword.getTopic();
+            targetNickname = latestCall.getTargetNickName();
+            targetProfileImage = latestCall.getTargetProfileImage();
         }
-
 
         return HomeSummaryDTO.builder()
                 .targetNickname(targetNickname)
